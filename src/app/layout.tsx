d27fcc880e66a63b9d70be8bea0fc481b14531c6
kurama0902 <creepysimbaplay@gmail.com>
2024-07-30 20:51:34 +0300

    test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>kemuri</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Playwrite+AT:ital,wght@0,100..400;1,100..400&display=swap" rel="stylesheet"></link>
+        <title>Kemuri</title>
       </head>
       <body id="top">
         <Header />
